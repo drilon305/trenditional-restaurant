67f9a2d449e32a6f9e98f395b127b4e241c588b0
Drilon <navycrack1@gmail.com>
2023-10-12 23:03:01 +0200

    implementing a sticky navigation

--- script.js
+++ script.js
@@ -31,3 +31,24 @@ allLinks.forEach((link) => {
       headerEl.classList.toggle("nav-open");
   });
 });
+
+
+
+const sectionHeroEl = document.querySelector('.section-hero')
+
+const obs = new IntersectionObserver((entries) => {
+  const ent = entries[0]
+  if(ent.isIntersecting === false){
+    document.body.classList.add('sticky')
+  }
+
+  if(ent.isIntersecting === true){
+    document.body.classList.remove('sticky')
+  }
+}, {
+  root: null,
+  threshold: 0,
+  rootMargin: '-80px'
+})
+
+obs.observe(sectionHeroEl)
